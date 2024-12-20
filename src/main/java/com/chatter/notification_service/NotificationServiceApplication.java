3fb43a788dda42dbb6484cede607fa3adc9a6b45
Maxim Serov <maxim58r@gmail.com>
2024-12-20 22:02:07 +0300

    commit checkstyle

--- src/main/java/com/chatter/notification_service/NotificationServiceApplication.java
+++ src/main/java/com/chatter/notification_service/NotificationServiceApplication.java
@@ -5,9 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NotificationServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(NotificationServiceApplication.class, args);
-	}
+    /**
+     * Entry point for the authentication service application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
 
 }
